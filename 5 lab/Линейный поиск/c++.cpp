edfeeddc74f26112d96b10f74ced42388c4f8767
artem ryzhkov <hekto.artem@yandex.ru>
2025-10-28 12:38:54 +0300

    Create c++.cpp

---
+++ 5 lab/Линейный поиск/c++.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int linearSearch(const vector<int>& arr, int target) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {3, 5, 2, 7, 9, 1, 4};
+    int target = 7;
+    int result = linearSearch(arr, target);
+    
+    if (result != -1) {
+        cout << "Элемент найден на позиции: " << result << endl;
+    } else {
+        cout << "Элемент не найден" << endl;
+    }
+    return 0;
+}
